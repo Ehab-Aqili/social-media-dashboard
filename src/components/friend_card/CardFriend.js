c069739e900f402d9b7b26dddc9fa1a3aeeadf9a
HakamQadi <hakam.h.qadi@gmail.com>
2023-08-05 22:24:29 +0300

    done

--- src/components/friend_card/CardFriend.js
+++ src/components/friend_card/CardFriend.js
@@ -5,10 +5,11 @@ import ImageUser from "../cache_image/ImageUser";
 import { PathIcons } from "../../util/PathIcons";
 const CardFriend = (props) => {
   const username = props.username
+  const image = props.image
   return (
     <div className="card_friend col-lg-5 p-4">
       <div className=" d-flex align-items-center gap-3">
-        <ImageUser image={userData[0].image} width={80} radius={"20%"} />
+        <ImageUser image={image} width={80} radius={"20%"} />
         <div>
           <span className="d-block">{username}</span>
           <span className="d-block mutual-text">{45} mutual friends</span>
--- src/components/friend_card/Friends.js
+++ src/components/friend_card/Friends.js
@@ -6,13 +6,13 @@ const Friends = () => {
   const data = JSON.parse(sessionStorage.getItem('user'))
   let newData = []
   newData.push(data)
-  // console.log(newData[0].friends)
+  console.log(newData[0].friends)
   return (
     <div className="row d-flex justify-content-center gap-2 p-3">
       {
         newData[0].friends.map((d, index) => {
           return  (
-            <CardFriend username={d.username} key={index} />
+            <CardFriend image={d.image} username={d.username} key={index} />
           )
         })
       }
